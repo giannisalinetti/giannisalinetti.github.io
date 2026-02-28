f7b4c53a9aafae8051bdf9ecc5f084b00f7d6b10
Gianni Salinetti <gsalinet@redhat.com>
2026-02-28 16:45:18 +0100

    Update configs to match the repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://giannisalinetti.github.io',
-  base: '/astro-demo',
   vite: {
     plugins: [tailwindcss()]
   }
